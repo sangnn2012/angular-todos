f30cc6e1329ff010b459bae0c2e67fa354f9440f
sangnn <sang.nguyen@eye-solution.vn>
2020-10-24 13:47:38 +0700

    angular service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Set Dynamic Classes
+  setClasses() {
+    const classes = {
+      todo: true,
+      'is-completed': this.todo.completed
+    };
+    return classes;
+  }
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+  onDelete(todo) {
+    
+  }
+
+}
